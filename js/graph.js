8e3361f575b2891453278e513bb9fbd62a6705b4
TheEverchosen <starobelsk97@gmail.com>
2018-11-23 14:51:42 +0000

    another one

--- js/graph.js
+++ js/graph.js
@@ -7,12 +7,11 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
         labels: valArray,
         datasets: [{
-            label: "Elevation graph",
+            label: "Height, m",
             backgroundColor: 'rgb(167, 232, 29)',
             borderColor: 'rgb(255, 99, 132)',
             data: elevations,
